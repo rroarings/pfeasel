c8cbe5489e8da68739529b26cda8e25c484f6baa
rroarings <deceivedfx@gmail.com>
2025-06-01 20:52:41 -0500

    Remove obsolete package-info.java file from app package

--- src/app/package-info.java
+++
@@ -1,7 +0,0 @@
-package app;
-
-// Placeholder for the new app package directory
-// This tool call is to ensure the directory actions/app exists if it doesn't
-// Actual file moving will be handled by subsequent calls or assumed to be done by the IDE/user
-// For the purpose of this automated interaction, we'll proceed as if files are moved,
-// and then update their package declarations and imports.
